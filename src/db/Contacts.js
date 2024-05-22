a6bc6a0d68967add5f5bf4212dbd4231e0ce8eed
spalnique <lex.sydoruk@gmail.com>
2024-05-22 23:18:12 +0200

    upd

---
+++ src/db/Contacts.js
@@ -0,0 +1,19 @@
+import { Schema, model } from 'mongoose';
+
+const contactsSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    email: { type: String, optional: true },
+    isFavourite: { type: Boolean, default: false },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const ContactsCollection = model('contacts', contactsSchema);
---
+++ src/server.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { env } from './utils/env.js';
+import { ENV_VARS } from './constants/index.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+export function setupServer() {
+  const app = express();
+  const PORT = Number(env(ENV_VARS.PORT, 3000));
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use(cors());
+
+  app.use(express.json());
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:id', async (req, res) => {
+    const { id } = req.params;
+    const contact = await getContactById(id);
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${id}`,
+      data: contact,
+    });
+  });
+
+  app.use('*', (res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
