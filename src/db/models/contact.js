7ec1e4c5c95b04b36e58614241fb6f01adf09d88
spalnique <lex.sydoruk@gmail.com>
2024-06-02 18:05:43 +0200

    .

---
+++ src/db/models/contact.js
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    email: { type: String, optional: true },
+    isFavourite: { type: Boolean, default: false },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const ContactsCollection = model('contact', contactSchema);
