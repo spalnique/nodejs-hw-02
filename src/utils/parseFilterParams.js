306cb89faac15a81b36969b0d24ab9705f357bc9
spalnique <lex.sydoruk@gmail.com>
2024-06-09 17:31:05 +0200

    parseBoolean implemented try..catch

--- src/utils/parseFilterParams.js
+++ src/utils/parseFilterParams.js
@@ -5,11 +5,21 @@ const parseContactType = (value) => {
   return value;
 };
 
-const parseBoolean = (value) => {
-  if (typeof value !== 'string') return;
-  if (!['true', 'false'].includes(value)) return;
+// const parseBoolean = (value) => {
+//   if (typeof value !== 'string') return;
+//   if (!['true', 'false'].includes(value)) return;
 
-  return JSON.parse(value);
+//   return JSON.parse(value);
+// };
+
+const parseBoolean = (value) => {
+  try {
+    const parsed = JSON.parse(value);
+    if (typeof parsed === 'boolean') return parsed;
+    return;
+  } catch (err) {
+    return;
+  }
 };
 
 export const parseFilterParams = (query) => {
