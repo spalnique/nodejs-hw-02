a536a03cc5766651bd3edc6f65522f880b847d5d
spalnique <136997639+spalnique@users.noreply.github.com>
2024-06-10 12:38:07 +0200

    Update contacts validation: min and max for name and phoneNumber

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,8 +1,8 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(2).required(),
-  phoneNumber: Joi.string().min(8).required(),
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().email().optional(),
   isFavourite: Joi.boolean().default(false),
   contactType: Joi.string()
@@ -12,8 +12,8 @@ export const createContactSchema = Joi.object({
 });
 
 export const patchContactSchema = Joi.object({
-  name: Joi.string().min(2),
-  phoneNumber: Joi.string().min(8),
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
