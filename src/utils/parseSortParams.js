e0d152acd884da0b5888b609a2023faae5ce8c76
spalnique <136997639+spalnique@users.noreply.github.com>
2024-06-10 12:32:30 +0200

    Update parseSortParams: Object.keys >> Object.values

--- src/utils/parseSortParams.js
+++ src/utils/parseSortParams.js
@@ -1,12 +1,12 @@
 import { KEYS_OF_CONTACT, SORT_ORDER } from '../constants/index.js';
 
 const parseSortOrder = (sortOrder) => {
-  if (Object.keys(SORT_ORDER).includes(sortOrder)) return sortOrder;
+  if (Object.values(SORT_ORDER).includes(sortOrder)) return sortOrder;
   return SORT_ORDER.ASC;
 };
 
 const parseSortBy = (sortBy) => {
-  if (Object.keys(KEYS_OF_CONTACT).includes(sortBy)) return sortBy;
+  if (Object.values(KEYS_OF_CONTACT).includes(sortBy)) return sortBy;
   return KEYS_OF_CONTACT._id;
 };
 
